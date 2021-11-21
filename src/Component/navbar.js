f9afa09d9f946381a70adf6b2dd26d1e489b7dfb
Thomas Jose <thomasjose875@gmail.com>
2021-11-21 17:49:04 +0530

    third navbar commit~

--- src/Component/navbar.js
+++ src/Component/navbar.js
@@ -9,7 +9,7 @@ export const Navbar = () => {
             <nav className="navbar navbar-expand-lg navbar-light bg-dark">
                 
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="https://www.google.com/">Thomas Jose</a>
+                    <a className="navbar-brand" href="#">Thomas Jose</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                     </button>
