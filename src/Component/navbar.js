b2ae3ab6fe28b75a7f67d638ee6eeb04b2985a1c
Thomas Jose <thomasjose875@gmail.com>
2021-11-21 17:42:49 +0530

    navbar second commit

--- src/Component/navbar.js
+++ src/Component/navbar.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import { Link } from "react-scroll";
+
+
 
 export const Navbar = () => {
     return (
@@ -11,25 +14,24 @@ export const Navbar = () => {
                     <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav ms-auto mb-2 mb-lg-0 ">
-                        <li className="nav-item">
-                        <a className="nav-link active" aria-current="page" href="https://www.google.com/" >Home</a>
+                    <ul className="navbar-nav ms-auto">
+                        <li className="nav-item active">
+                        <Link smooth={true} to="home" className="nav-link" href="#">Home <span className="sr-only"></span></Link>
                         </li>
                         <li className="nav-item">
-                        <a className="nav-link" href="https://www.google.com/">About me</a>
+                        <Link smooth={true} to="about" offset={-110} className="nav-link" >about me</Link>
                         </li>
                         <li className="nav-item">
-                        <a className="nav-link active" aria-current="page" href="https://www.google.com/" >Projects</a>
+                        <Link smooth={true} to="experience" offset={-110} className="nav-link" >experience</Link>
                         </li>
                         <li className="nav-item">
-                        <a className="nav-link active" aria-current="page" href="https://www.google.com/" >Portfolio</a>
+                        <Link smooth={true} to="portfolio" offset={-110} className="nav-link" >portfolio</Link>
                         </li>
+
                         <li className="nav-item">
-                        <a className="nav-link active" aria-current="page" href="https://www.google.com/" >contacts</a>
+                        <Link smooth={true} to="contacts" offset={-110} className="nav-link" >contacts</Link>
                         </li>
-                       
                     </ul>
-                    
                     </div>
                 </div>
             </nav>
