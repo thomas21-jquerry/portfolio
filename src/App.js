2e39381d79da00fb6db424eceb7b737ce9bff47c
Thomas Jose <thomasjose875@gmail.com>
2021-12-18 15:29:28 +0530

    cursor done

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
+import React,{useState} from 'react';
 import Navbar from './Component/navbar';
 import Header from "./Component/header"
 
+
+
 function App() {
+  const [cursorX, setCursorX] = useState()
+  const [cursorY, setCursorY] = useState()
+
+  window.addEventListener('mousemove',e => {
+    setCursorX(e.pageX)
+    setCursorY(e.pageY)
+  })
+
   return (
-    
+     
+
+      
       <div className="outerBody">
+         
+        
         <div className="navbarcolor">
           <Navbar />
         </div>
         <Header />
-        
+        <div className='cursor' style={{
+        left: cursorX +"px",
+        top: cursorY+"px"
+      }}> </div>
         
       </div>
     
