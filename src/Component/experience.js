d94dcfdc151c23ff2cce4120835c248d4c743b2e
Thomas Jose <thomasjose875@gmail.com>
2022-01-22 18:31:21 +0530

    skill done

--- src/Component/experience.js
+++ src/Component/experience.js
@@ -1,15 +1,37 @@
-import React from 'react';
+import React , {Component} from 'react';
 
-export const Experience = () => {
-    
-    return (
-        <div className='Experience' id='#Experience'>
-            <div className='ExpereinceContainer'>
-                <h2>Experience & Skills</h2>
-                
+
+export default class Experience extends Component{
+    constructor(){
+        super();
+    }
+
+
+
+    render() {
+        return (
+            <div className='Skill' id='#Experience'>
+              
+            <div className='SkillContainer'>
+                <h1>Skills</h1>
+                <br />
+                <hr />
+                <div>
+                    < button class="button-33" role="button">Node</button>
+                    < button class="button-33" role="button">React</button>
+                    < button class="button-33" role="button">Python</button>
+                    < button class="button-33" role="button">Flask</button>
+                    < button class="button-33" role="button">Solidity</button>
+                    < button class="button-33" role="button">Blockchain</button>
+                </div>
+                {/* <div id='ExperienceText'>
+                    <Ring /> 
+                    <h4>hellow</h4>
+                </div> */}
             </div>
             
         </div>
-    )
+        );
+       
+    }
 }
-export default Experience;
